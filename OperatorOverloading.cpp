50b74ec4fb183a8f2831fe901eaefc945053caf7
Raaj-Chimulkar <141911872+Raaj-Chimulkar@users.noreply.github.com>
2023-12-12 19:44:25 +0530

    Add files via upload

--- OperatorOverloading.cpp
+++ OperatorOverloading.cpp
@@ -1,229 +1,43 @@
-
-/*
-In BigInt class overload following operators:
-   Binary operators + - * / to work with
-   - both operands of type BigInt
-   - one BigInt operand and one int
-   Unary operators ++ and -- in both pre and post form.
-   Also write a small program to demonstrate the use of BigInt class and overloaded operators.
-*/
-
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-class BigInt
+
+class bigint
 {
-    int num;
+    int i;
 
-public:
-    void get()
+    public:
+    bigint(int a=0)
     {
-        cout << num << endl;
-    }
-    BigInt()
-    {
-        num = 0;
-        // cout << "parameter less constructor" << endl;
-    }
-    BigInt(int num)
-    {
-        this->num = num;
-        // cout << "parameterized constructor" << endl;
+        i=a;
     }
 
-    // overload + operator
-    BigInt operator+(BigInt obj)
+    bigint operator+(bigint &obj)
     {
-        BigInt result;
-        result.num = this->num + obj.num;
-        return result;
-    }
-    //  overload + int operator
-    BigInt operator+(int value)
-    {
-        BigInt result;
-        result.num = this->num + value;
+        bigint result;
+        cout<<"value of i: "<<i<<endl;
+        cout<<"value of obj.i: "<<obj.i<<endl;
+        result.i = this->i + obj.i;
         return result;
     }
 
-    // overload - operator
-    BigInt operator-(BigInt obj)
+    void print()
     {
-        BigInt result;
-        result.num = this->num - obj.num;
-        return result;
-    }
-
-    // overload - int operator
-    BigInt operator-(int value)
-    {
-        BigInt result;
-        result.num = this->num - value;
-        return result;
-    }
-
-    // overload  / operator
-    BigInt operator/(BigInt obj)
-    {
-        BigInt result;
-        result.num = this->num / obj.num;
-        return result;
-    }
-
-    // overload / int operator
-    BigInt operator/(int value)
-    {
-        BigInt result;
-        result.num = this->num / value;
-        return result;
-    }
-
-    // overload  * operator
-    BigInt operator*(BigInt obj)
-    {
-        BigInt result;
-        result.num = this->num * obj.num;
-        return result;
-    }
-
-    // overload * int operator
-    BigInt operator*(int value)
-    {
-        BigInt result;
-        result.num = this->num * value;
-        return result;
-    }
-
-    // Overload ++ prefix operator
-    BigInt operator++()
-    {
-        BigInt result;
-        result.num = ++this->num;
-        return result;
-    }
-
-    // Overload ++ postfix operator
-    BigInt operator++(int)
-    {
-        BigInt result;
-        result.num = this->num++;
-        return result;
-    }
-
-    // Overload -- prefix operator
-    BigInt operator--()
-    {
-        BigInt result;
-        result.num = --this->num;
-        return result;
-    }
-
-    // Overload -- postfix operator
-    BigInt operator--(int)
-    {
-        BigInt result;
-        result.num = this->num--;
-        return result;
+        cout<<"addition: "<<i<<endl;
     }
 };
 
 int main()
 {
-    BigInt bigA(5), bigB(6), bigC;
-    int a = 3;
+    bigint obj1(101);
+    bigint obj2(305);
+    // bigint obj3(100);
+    bigint res ; 
 
-    // addition +
-    BigInt sum = bigA + a; // with int
-    cout << "addition bigA(5) + 3 :";
-    sum.get();
+    res = obj1 + obj2;
+    res.print();
     cout<<endl;
 
-    BigInt bigsum1 = bigA + bigB; // with object
-    cout << "addition bigA(5) + bigB(6) :";
-    bigsum1.get();
-    cout<<endl;
 
-    // substraction -
-    BigInt sub = bigA - a; // with int
-    cout << "substraction bigA(5) - 3 :";
-    sub.get();
-    cout<<endl;
-
-    BigInt bigsub1 = bigA - bigB; // with object
-    cout << "substraction bigA(5) - bigB(6) :";
-    bigsub1.get();
-    cout<<endl;
-
-    // multiplication *
-    BigInt mul = bigA * a; // with int
-    cout << "multiplicastion bigA(5) * 3 :";
-    mul.get();
-    cout<<endl;
-
-    BigInt bigmul1 = bigA * bigB; // with object
-    cout << "substraction bigA(5) * bigB(6) :";
-    bigmul1.get();
-    cout<<endl;
-
-    // division /
-    BigInt div = bigA / a; // with int
-    cout << "division bigA(5) / 3 :";
-    div.get();
-    cout<<endl;
-
-    BigInt div1 = bigA / bigB; // with int
-    cout << "substraction bigA(5) / bigB(6) :";
-    div1.get();
-    
-
-    cout<<endl<<endl;
-    cout<<"Before preincrement bigC = ++bigA;  "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-    bigC = ++bigA;
-    cout<<"After preincrement bigC = ++bigA  "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-
-    cout<<"Before postincrement bigC = bigA++ "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-    bigC = bigA++;
-    cout<<"After postincrement bigC = bigA++ "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-
-    cout<<"Before predecrement bigC = --bigA "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-    bigC = --bigA;
-    cout<<"After predecrement bigC = --bigA "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-
-    cout<<"Before postdecrement bigC = bigA-- "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
-    bigC = bigA--;
-    cout<<"After postdecrement bigC = bigA-- "<<endl;
-    cout<<"bigC : ";
-    bigC.get();
-    cout<<"bigA : ";
-    bigA.get();
 
     return 0;
 }
